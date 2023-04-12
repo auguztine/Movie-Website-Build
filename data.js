0ad6a934c9dbf39b97c711033f7a7d7b470dce38
auguztine <ampofoaugustine0@gmail.com>
2023-04-12 12:28:16 +0000

    Added Data js

---
+++ data.js
@@ -0,0 +1,24 @@
+const movieData = [
+    {
+        img: "./img/img1.jpg",
+        movieName: "Gangs of London",
+        text: "asjdhbalsjdb"
+    },
+    {
+        img: "./img/img2.jpg",
+        movieName: "Snowwfall",
+        text: "asjdhbalsjdb"
+    },
+    {
+        img: "./img/img3.jpg",
+        movieName: "All American",
+        text: "asjdhbalsjdb"
+    },
+    {
+        img: "./img/img4.jpg",
+        movieName: "God father of Harlem",
+        text: "asjdhbalsjdb"
+    }
+];
+
+export default movieData;
